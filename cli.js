c4fbf4996f6a05ac6e2ea5a87a6d8728befc9294
Greg Richardson <greg@patronscan.com>
2017-08-17 14:21:24 -0600

    Fixed readStream() stream reference and added data handler

--- cli.js
+++ cli.js
@@ -1,16 +1,21 @@
-const { print: ninprint } = require("./");
 
-// Read a stream and return a buffer
-const readStream = stream => new Promise((resolve, reject) => {
+const { print } = require("./");
+
+// Read a stream and return a buffer when it ends
+const readStream = (stream, dataHandler) => new Promise((resolve, reject) => {
 
 	const buffers = [];
 
-	process.stdin.on("data", data => buffers.push(data));
-	process.stdin.on("error", error => reject(error));
-	process.stdin.on("end", () => resolve(Buffer.concat(buffers)));
+	stream.on("data", data => {
+
+		buffers.push(data);
+		
+		if (dataHandler)
+			dataHandler(data);
+	});
+	stream.on("error", error => reject(error));
+	stream.on("end", () => resolve(Buffer.concat(buffers)));
 });
 
-// Read from STDIN and print the symbols
-readStream(process.stdin).then(data => {
-	ninprint(data);
-});
+// Read from STDIN and print as they arrive
+readStream(process.stdin, data => print(data));
